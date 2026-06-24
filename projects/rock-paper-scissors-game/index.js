13e3a0a3882ff8af95bc5ee3c6dc58b790274047
kishi950929-dot <kishi950929@gmail.com>
2026-06-24 22:50:18 +0800

    new looking of rock paper scissors

--- projects/rock-paper-scissors-game/index.js
+++ projects/rock-paper-scissors-game/index.js
@@ -1,19 +1,52 @@
 const buttons = document.querySelectorAll("button");
-
 const resultEl = document.getElementById("result");
-
 const playerScoreEl = document.getElementById("user-score");
-
 const computerScoreEl = document.getElementById("computer-score");
+const playerChoiceEl = document.getElementById("player-choice");
+const computerChoiceEl = document.getElementById("computer-choice");
+
+const choiceIcons = {
+  rock: "✊",
+  paper: "🖐",
+  scissors: "✌️",
+};
 
 let playerScore = 0;
 let computerScore = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const result = playRound(button.id, computerPlay());
-    resultEl.textContent = result;
+
+    const playerChoice = button.id;
+    const computerChoice = computerPlay();
+
+    playerChoiceEl.textContent = choiceIcons[playerChoice];
+    computerChoiceEl.textContent = choiceIcons[computerChoice];
+
+    playerChoiceEl.classList.remove("animate-choice");
+    computerChoiceEl.classList.remove("animate-choice");
     
+    setTimeout(() => {
+      playerChoiceEl.classList.add("animate-choice");
+      computerChoiceEl.classList.add("animate-choice");
+    }, 10);
+    //内置函数，要执行的代码+等待时间（ms），实际上是为了remove刷新
+    //"animate-choice"是在css里定义的一个class样式
+
+    playerChoiceEl.classList.remove("winner");
+    computerChoiceEl.classList.remove("winner");
+
+    const result = playRound(playerChoice, computerChoice);
+    resultEl.textContent = result.message;
+
+    if (result.winner === "player") {
+        playerChoiceEl.classList.add("winner");
+    }
+
+    if (result.winner === "computer") {
+        computerChoiceEl.classList.add("winner");
+    }
+        
   });
 });
 
@@ -25,7 +58,10 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
+    return {
+      winner: "tie",
+      message: "It's a tie!"
+    }
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
@@ -33,10 +69,15 @@ function playRound(playerSelection, computerSelection) {
   ) {
     playerScore++;
     playerScoreEl.textContent = playerScore;
-    return "You win! " + playerSelection + " beats " + computerSelection;
+    return {
+      winner: "player",
+      message: "You win! " + playerSelection + " beats " + computerSelection
+    }
   } else {
     computerScore++;
     computerScoreEl.textContent = computerScore;
-    return "You lose! " + computerSelection + " beats " + playerSelection;
-  }
-}
+    return {
+      winner: "computer",
+      message: "You lose! " + computerSelection + " beats " + playerSelection
+    }
+  }}
